4b485fe0e65c2c72eb403f97e4a338c439714928
AssadullahMalik <asadullahmk102@gmail.com>
2026-02-23 21:46:08 +0500

    Update 02_variables_and_data_types.cpp

---
+++ 02_variables_and_data_types.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 25;
+    double height = 5.9;
+    char grade = 'A';
+    bool isStudent = true;
+
+    cout << "Age: " << age << "\nHeight: " << height << "\nGrade: " << grade << "\nStudent: " << isStudent << endl;
+    return 0;
+}
